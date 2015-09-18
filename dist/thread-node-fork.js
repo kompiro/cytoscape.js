7a54ed775fe4bd55fd9f569fd44136f52f8ec38b
Hiroki Kondo <kompiro@gmail.com>
2015-09-19 01:56:33 +0900

    update dist for test to use

--- dist/thread-node-fork.js
+++ dist/thread-node-fork.js
@@ -1,20 +1,21 @@
 /*!
- * This file is part of Cytoscape.js 2.4.0-unstable.
- * 
+ * This file is part of Cytoscape.js 2.5.0-kompiro.
+ *
  * Cytoscape.js is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * Cytoscape.js is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with
  * Cytoscape.js. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+/* jshint ignore:start */
 
 // normalised thread api functions for nodejs
 
@@ -35,7 +36,7 @@ function listen( fn ){
     } else {
       fn( m );
     }
-  });  
+  });
 }
 
 function resolve( v ){
@@ -54,4 +55,6 @@ process.on('message', function( m ){
   if( typeof m === 'object' && m.$$eval ){
     eval( m.$$eval );
   }
-}); 
+});
+
+/* jshint ignore:end */
